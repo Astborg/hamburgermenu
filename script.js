e4a8b147f09a87a31970195b43e7ec5bab11d63e
Astborg <93790636+Astborg@users.noreply.github.com>
2023-03-03 19:49:34 +0100

    changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//dark mode
 const bar = document.getElementById("bar");
 const menu = document.getElementById("menu");
 const change = document.getElementById("change");
@@ -9,7 +10,6 @@ const pris = document.querySelector('.pris')
 
 
 bar.addEventListener("click", () => {
-  // menu.style.display = 'flex'
   menu.classList.toggle("hidden");
 });
 change.addEventListener("click", () => {
@@ -20,9 +20,7 @@ change.addEventListener("click", () => {
     change.innerText = "light";
     change.style.color = "white";
     change.style.backgroundColor = "black";
-    // image.style.backgroundColor = 'black'
-    // beskrivning.style.backgroundColor = 'black'
-    // pris.style.backgroundColor = 'black'
+
     square.style.backgroundColor = 'black'
   } else {
   mode.content = "light";
@@ -36,60 +34,36 @@ change.addEventListener("click", () => {
 }
 });
 
-const home = document.getElementById('home')
-const about = document.getElementById('about')
-const contact = document.getElementById('contact')
-const language1 = document.getElementById('language1')
-const lang1 = document.getElementById('lang1')
-const euro = document.querySelectorAll('.euro')
-const text = document.querySelectorAll('.text')
-const buy = document.querySelectorAll('.buy')
-const webshop = document.getElementById('webshop')
+//hämta från firebase
+async function getProducts(){
+  try{
+    const baseURL = 'https://webstore-22fa4-default-rtdb.europe-west1.firebasedatabase.app/'
+    const url = baseURL + 'Products.json'
+    const response = await fetch(url)
+    let data = await response.json()
+    console.log(data)
 
-if((navigator.language.match("en-US") || navigator.language.match("en"))){
-  language1.innerText = 'engelska'
-}else{
-  language1.innerText = 'svenska'
-}
-language1.addEventListener('click', () =>{
-  console.log(language1.innerText)
-if (language1.innerText === 'svenska'){
-  console.log('svenska')
-  language1.innerText = 'engelska'
-  home.innerText = 'Hem'
-  about.innerText = 'Om oss'
-  contact.innerText = 'Kontakt'
-  webshop.innerText = 'E-handel'
-  buy.forEach(element => {
-    element.innerText = 'Köp'
-  });
-  euro.forEach(element => {
-    element.innerText = '599kr'
-  });
-  text.forEach(element => {
-    element.innerText = 'Beskrivning'
-  });
-  
-
-} 
-
-else if (language1.innerText === 'engelska'){
-  language1.innerText = 'svenska'
-  console.log('svenska')
-  home.innerText = 'Home'
-  about.innerText = 'About'
-  contact.innerText = 'Contact'
-  webshop.innerText = 'Webshop'
-  buy.forEach(element => {
-    element.innerText = 'Buy'
-  });
-  euro.forEach(element => {
-    element.innerText = '59$'
-  });
-  text.forEach(element => {
-    element.innerText = 'Description'
-  });
+    // const generatedHTML = Object.keys(data).reduce((accum, currKey) => accum +
+    const generatedHTML = Object.keys(data).reduce((accum, i) => accum +
+    
+    `<div>
+      <div>${data[i].namn}</div>
+      <div class="image"><img class="skor-img" src="${data[i].img}" alt=""></div>
+      <div class="beskrivning"><strong class="text">Description</strong><br><br><p class="text2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum quas quasi praesentium voluptates minima deleniti enim sint quidem expedita sed ut officiis hic dolore dolor quaerat atque earum tenetur necessitatibus, maiores odit illo molestias voluptatibus adipisci quos? Vero necessitatibus tempora inventore, accusamus illo, maiores quisquam quo odit nesciunt qui nisi hic est non aliquid ex numquam earum aperiam? Recusandae excepturi eligendi nemo magni praesentium assumenda, placeat rem numquam illo provident earum officia eveniet tempora commodi laborum molestias animi expedita nihil sint explicabo quos modi at. Odio vel a voluptas explicabo pariatur, enim impedit quas eius ab recusandae, veniam, deserunt consequatur.</p></div>
+      <div class="pris"><p>lager: ${data[i].lager}</p> <button class="euro">Buy:${data[i].pris}kr</button></div></div>`
+    ,'' )
+      
+    document.getElementById('container').innerHTML =generatedHTML 
+  }catch(error){
+    console.log(error)
+  }
   
 }
 
-})
+getProducts()
+
+
+  
+
+
+
